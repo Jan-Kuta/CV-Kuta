b8b1c84535bbebe322a1d140780e4fae16343968
Jan-Kuta <jan.kuta@email.cz>
2020-06-30 06:47:09 +0200

    Replaced bugged hook for media queries

--- components/cv.js
+++ components/cv.js
@@ -1,21 +1,23 @@
+import React from 'react'
 import PersonalInfo from './personalInfo'
 import Skills from './skills'
 import Work from './workExperience'
-import Education from './education';
-import Hobbies from './hobbies';
-import { useMediaQuery } from "@react-hook/media-query";
+import Education from './education'
+import Hobbies from './hobbies'
+import useSize  from "@react-hook/size"
 
 const CV = () => {
-    const matches = useMediaQuery('only screen and (min-width: 600px)')
+    const target = React.useRef(null)
+    const [width] = useSize(target)
+    const matches = width < 800
 
     return (
-        <div className="w3-content" style={{maxWidth: "1400px"}} >
-
+        <div className="w3-content" style={{maxWidth: "1400px"}} ref={target}>
             {/* The Grid */}
-            <div className="w3-row-padding" style={{display: "flex", flexFlow: matches ? "row" : "column"}}>
+            <div className="w3-row-padding" style={{display: "flex", flexFlow: matches? "column" : "row"}}>
 
                 {/* Left Column */}
-                <div className={matches ? "w3-margin" : "w3-margin-top"} style={{flexGrow: 1}}>
+                <div className={matches ? "w3-margin-top" : "w3-margin"} style={{flexGrow: 1}}>
                     <div className="w3-white w3-text-grey w3-card" style={{height: "100%"}}>
                         <PersonalInfo />
                         <hr />
